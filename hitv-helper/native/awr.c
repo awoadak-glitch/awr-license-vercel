db04d3d8bd1bfe8e3b0184c6ef4d8027d10c45ee
awoadak-glitch <awoadak@gmail.com>
2026-08-22 16:35:06 -0700

    Fully neutralize TraidMod updater JNI methods

--- hitv-helper/native/awr.c
+++ hitv-helper/native/awr.c
@@ -1,44 +1,47 @@
 #include <jni.h>
 
-/* AWR HiTV helper: VIP gate + TraidMod update suppression without touching HiTV DEX/native code. */
+/* AWR HiTV helper: VIP gate + complete TraidMod updater neutralization. */
 static volatile jint g_vip_enabled = 0;
 
 static jboolean JNICALL awrVipGate(JNIEnv *env, jobject thiz) {
-    (void)env;
-    (void)thiz;
+    (void)env; (void)thiz;
     return g_vip_enabled ? JNI_TRUE : JNI_FALSE;
 }
 
 static void JNICALL awrNoopVoid(JNIEnv *env, jobject thiz) {
-    (void)env;
-    (void)thiz;
+    (void)env; (void)thiz;
 }
 
 static void JNICALL awrNoopContext(JNIEnv *env, jobject thiz, jobject context) {
-    (void)env;
-    (void)thiz;
-    (void)context;
+    (void)env; (void)thiz; (void)context;
 }
 
 static void JNICALL awrNoop3Strings(JNIEnv *env, jobject thiz, jstring a, jstring b, jstring c) {
-    (void)env;
-    (void)thiz;
-    (void)a;
-    (void)b;
-    (void)c;
+    (void)env; (void)thiz; (void)a; (void)b; (void)c;
 }
 
-static jobject JNICALL awrNoopVoidArray(JNIEnv *env, jobject thiz, jobjectArray values) {
-    (void)env;
-    (void)thiz;
-    (void)values;
+static jobject JNICALL awrReturnNull1(JNIEnv *env, jobject thiz, jobject arg) {
+    (void)env; (void)thiz; (void)arg;
+    return NULL;
+}
+
+static jobject JNICALL awrReturnNullStatic1(JNIEnv *env, jclass clazz, jobject arg) {
+    (void)env; (void)clazz; (void)arg;
+    return NULL;
+}
+
+static jint JNICALL awrReturnZero1(JNIEnv *env, jobject thiz, jobject arg) {
+    (void)env; (void)thiz; (void)arg;
+    return 0;
+}
+
+static jobject JNICALL awrReturnNullVoidArray(JNIEnv *env, jobject thiz, jobjectArray values) {
+    (void)env; (void)thiz; (void)values;
     return NULL;
 }
 
 static void JNICALL awrNoopJson(JNIEnv *env, jobject thiz, jobject value) {
-    (void)env;
-    (void)thiz;
-    (void)value;
+    (void)env; (void)thiz; (void)value;
 }
 
 static jboolean disableTraidUpdate(JNIEnv *env) {
@@ -49,11 +52,15 @@ static jboolean disableTraidUpdate(JNIEnv *env) {
     if (upd != NULL) {
         JNINativeMethod methods[] = {
             {"chk", "(Landroid/content/Context;)V", (void *)awrNoopContext},
+            {"getInst", "(Landroid/content/Context;)Lcom/extreme/modding/Upd8Chck;", (void *)awrReturnNullStatic1},
+            {"x", "()V", (void *)awrNoopVoid},
+            {"pkgInfo", "(Landroid/content/pm/PackageManager;)Landroid/content/pm/PackageInfo;", (void *)awrReturnNull1},
             {"shw", "()V", (void *)awrNoopVoid},
             {"shwUpd8Dlg", "(Ljava/lang/String;Ljava/lang/String;Ljava/lang/String;)V", (void *)awrNoop3Strings},
-            {"x", "()V", (void *)awrNoopVoid}
+            {"vCode", "(Landroid/content/pm/PackageInfo;)I", (void *)awrReturnZero1}
         };
-        if ((*env)->RegisterNatives(env, upd, methods, 4) == 0) changed = JNI_TRUE;
+        if ((*env)->RegisterNatives(env, upd, methods, (jint)(sizeof(methods) / sizeof(methods[0]))) == 0)
+            changed = JNI_TRUE;
         if ((*env)->ExceptionCheck(env)) (*env)->ExceptionClear(env);
         (*env)->DeleteLocalRef(env, upd);
     } else if ((*env)->ExceptionCheck(env)) {
@@ -63,10 +70,12 @@ static jboolean disableTraidUpdate(JNIEnv *env) {
     jclass task = (*env)->FindClass(env, "com/extreme/modding/Upd8Chck$CkUpd8Task");
     if (task != NULL) {
         JNINativeMethod taskMethods[] = {
-            {"doInBackground", "([Ljava/lang/Void;)Lorg/json/JSONObject;", (void *)awrNoopVoidArray},
+            {"access$0", "(Lcom/extreme/modding/Upd8Chck$CkUpd8Task;)Lcom/extreme/modding/Upd8Chck;", (void *)awrReturnNullStatic1},
+            {"doInBackground", "([Ljava/lang/Void;)Lorg/json/JSONObject;", (void *)awrReturnNullVoidArray},
             {"onPostExecute", "(Lorg/json/JSONObject;)V", (void *)awrNoopJson}
         };
-        if ((*env)->RegisterNatives(env, task, taskMethods, 2) == 0) changed = JNI_TRUE;
+        if ((*env)->RegisterNatives(env, task, taskMethods, (jint)(sizeof(taskMethods) / sizeof(taskMethods[0]))) == 0)
+            changed = JNI_TRUE;
         if ((*env)->ExceptionCheck(env)) (*env)->ExceptionClear(env);
         (*env)->DeleteLocalRef(env, task);
     } else if ((*env)->ExceptionCheck(env)) {
@@ -79,9 +88,8 @@ static jboolean disableTraidUpdate(JNIEnv *env) {
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM *vm, void *reserved) {
     (void)reserved;
     JNIEnv *env = NULL;
-    if (vm == NULL || (*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_6) != JNI_OK) {
+    if (vm == NULL || (*vm)->GetEnv(vm, (void **)&env, JNI_VERSION_1_6) != JNI_OK)
         return JNI_VERSION_1_6;
-    }
     disableTraidUpdate(env);
     return JNI_VERSION_1_6;
 }
@@ -121,8 +129,8 @@ Java_com_awr_license_AwrLicenseInitializationContentProviderForHiTV2026_nativeSe
         {"vipEnableTogetherVoice", "()Z", (void *)awrVipGate}
     };
 
-    jint count = (jint)(sizeof(methods) / sizeof(methods[0]));
-    jint rc = (*env)->RegisterNatives(env, userState, methods, count);
+    jint rc = (*env)->RegisterNatives(env, userState, methods,
+            (jint)(sizeof(methods) / sizeof(methods[0])));
     if (rc != 0) {
         if ((*env)->ExceptionCheck(env)) (*env)->ExceptionClear(env);
         (*env)->DeleteLocalRef(env, userState);
