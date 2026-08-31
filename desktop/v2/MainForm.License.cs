15137908e326a6029044b2cd5eeaebbf1d830383
awoadak-glitch <awoadak@gmail.com>
2026-08-30 21:35:35 -0700

    Fix Mustaqbal v2 Windows Forms reference

--- desktop/v2/MainForm.License.cs
+++ desktop/v2/MainForm.License.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using Microsoft.Win32;
 
 namespace AlMustaqbal
