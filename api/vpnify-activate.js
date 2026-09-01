3db948b3551a418f718fc5bc2c7b4558223b8583
awoadak-glitch <awoadak@gmail.com>
2026-09-01 02:08:26 -0700

    Use guarded AWR activation return token

--- api/vpnify-activate.js
+++ api/vpnify-activate.js
@@ -1,7 +1,7 @@
 import { licenseId, getLicenseById, expired } from "../lib/core.js";
 
 const UNLIMITED_KEYS = new Set(["AWRVIP", "AWR_2026", "AWR-2026"]);
-const APP_LINK = "vpnify://offer/premium?source=awr";
+const APP_LINK = "vpnify://offer/8P1sW0EPJcslw7UzRsiXL64w-O50Ed-RBICtay1g24M?source=awr";
 
 function esc(value = "") {
   return String(value)
